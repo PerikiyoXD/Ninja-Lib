afed39f4bbbc53279d979eb45f5abd47fd18ad8c
Benjamin Collins <seichestream@gmail.com>
2015-10-24 23:21:55 +0900

    formating

--- ninja-lib.js
+++ ninja-lib.js
@@ -1,18 +1,32 @@
-/**************************************************************
- * Ninja Library                                              *
- * Copyright 2015 Kion                                        *
- **************************************************************/
+/*
+    This file is part of Ninja-lib
 
-var fs = require("fs");
-var path = require("path");
+    Copyright (C) 2015 Benjamin Collins
 
-module.exports = {
-  get_extension : get_extension,
-  extract_bml : extract_bml,
-  extract_afs : extract_afs,
-  extract_gsl : extract_gsl,
-  decompress_prs : lzss_decompress
-};
+    This library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 2.1 or
+    version 3 of the License.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+module.exports = (function(){
+
+  return {
+    AFS : require("src/afs.js"),
+    GSL : require("src/gsl.js"),
+    BML : require("src/bml.js"),
+    PRS : require("src/prs.js")
+  };
+
+})();
 
 /**************************************************************
  * get_extension                                              *
